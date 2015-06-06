ac50c2a4a9ebca6026e745aa06ea6f421b36309f
fortachong <darkforta@gmail.com>
2015-06-06 16:58:56 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,21 @@
+# Author: Jorge Chong
+# Set Language to English
+Sys.setlocale("LC_TIME", "English")
+# Download the file from the official URL
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              method="auto", destfile = "data.zip")
 
-setwd("E:/Coursera/Data")
-
-plotData <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, 
+# Import to data frame plotData, using unzip to decompress file
+plotData <- read.table(unzip("data.zip", "household_power_consumption.txt"), sep = ";", header = TRUE, 
                        colClasses = c("character","character","character",
                                       "character","character","character",
                                       "character","character","character"))
+
+
+# Open png file
+png(file="plot1.png", width=480, height=480)
+
 toplot <- plotData[plotData$Date == "1/2/2007" | plotData$Date == "2/2/2007",]
 toplot$Global_active_power <- as.numeric(toplot$Global_active_power)
 
@@ -12,3 +23,5 @@ hist(toplot$Global_active_power,
      main = "Global Active Power", 
      col = "red", 
      xlab = "Global Active Power (kilowatts)")
+# Close file
+dev.off()
